acb62dbd5f2fa36d5b52c2a09bbc5191692f6fcd
dfingerman <dmitriy.fingerman@esignlive.com>
2017-11-14 22:59:34 -0500

    Random page graph generator

---
+++ src/main/java/pagerank/PageGraphGenerator.java
@@ -0,0 +1,58 @@
+package pagerank;
+
+import org.apache.commons.lang3.RandomStringUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class PageGraphGenerator {
+
+    public PageGraphGenerator(){}
+
+    public void generate(String outputFileName, int size, int averageNumberOfOutboundPages, int standardDeviationOutbounfPages){
+
+        List<Page> pages = new ArrayList<>();
+        Random randomGenerator = new Random();
+
+        for (int i=0; i<size; i++){
+
+            String name = RandomStringUtils.randomAlphabetic(10);
+            Page page = new Page("www." + name + ".com");
+            pages.add(page);
+        }
+
+        for (Page page : pages){
+
+            int outboundPagesCount = (int)(randomGenerator.nextGaussian()*standardDeviationOutbounfPages + averageNumberOfOutboundPages);
+
+            for (int j=0; j<outboundPagesCount; j++){
+
+                int pageNumber = randomGenerator.nextInt(pages.size());
+
+                if (page.getOutboundPages().contains(pages.get(pageNumber)) ||
+                        page.getName().equals(pages.get(pageNumber).getName())){
+                    j--;
+                    continue;
+                }
+
+                page.addOutboundPage(pages.get(pageNumber));
+            }
+        }
+
+        List<String> lines = new ArrayList<>();
+
+        for (Page page : pages){
+            for (Page outboundPage : page.getOutboundPages()){
+                lines.add(page.getName() + ", " + outboundPage.getName());
+            }
+        }
+
+        FileUtils.listToFile(outputFileName, lines);
+    }
+
+    public static void main(String[] args){
+        PageGraphGenerator pageGraphGenerator = new PageGraphGenerator();
+        pageGraphGenerator.generate("aa", 100, 5, 3 );
+    }
+}
